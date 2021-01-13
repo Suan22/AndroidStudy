64252a3aeb4b2b8a5a383c0b911464eacb24d855
Suan22 <tndks0770@naver.com>
2021-01-13 23:14:00 +0900

    1월 13일 커밋

--- Jan_assignment/app/src/main/java/com/example/jan_assignment/MainActivity.kt
+++ Jan_assignment/app/src/main/java/com/example/jan_assignment/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.example.jan_assignment
 
+import android.Manifest
 import android.app.Activity
 import android.app.Instrumentation
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import android.view.View
+import androidx.annotation.NonNull
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.jan_assignment.databinding.ActivityMainBinding
-import java.lang.Exception
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val REQUEST_READ_EXTERMAL_STORAGE = 1000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -22,8 +29,84 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
+        // 권한 부여 확인
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            // 권한 허용 안됨
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+                // 이전에 이미 권한이 거부되었을 때 설명
+                alert("사진 정보를 얻으려면 외부 저장소 권한이 필수로 필요합니다",
+                        "권한이 필요한 이유") {
+                    yesButton {
+                        // 권한 요청
+                        ActivityCompat.requestPermissions(this@MainActivity,
+                                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                                REQUEST_READ_EXTERMAL_STORAGE)
+                    }
+                    noButton {  }
+                }.show()
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                        REQUEST_READ_EXTERMAL_STORAGE)
+            }
+        } else {
+            // 권한이 이미 허용됨
+
+            getAllPhotos()
+
+        }
     }
 
+
+    // 사용자가 권한 요청 시 호출되는 메서드
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            REQUEST_READ_EXTERMAL_STORAGE -> {
+                if((grantResults.isNotEmpty() && grantResults[0]
+                                == PackageManager.PERMISSION_GRANTED)) {
+                    getAllPhotos()
+                } else {
+                    toast("권한 거부 됨")
+                }
+                return
+            }
+        }
+    }
+
+    private fun getAllPhotos(){
+        val cursor = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null,
+                null,
+                null,
+                MediaStore.Images.ImageColumns.DATE_TAKEN + " DESC")
+        val uriArr = ArrayList<String>()
+        if(cursor!=null){
+            while(cursor.moveToNext()){
+                // 사진 경로 Uri 가져오기
+                val uri = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA))
+                uriArr.add(uri)
+            }
+            cursor.close()
+        }
+
+        val adapter = MyAdapter(this,uriArr)
+
+            binding.gridview.numColumns=3 // 한 줄에 3개씩
+            binding.gridview = adapter
+    }
+
+
+}
+
+
+
+
+/*
     fun LoadImage(v:View){
         startActivityForResult(Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI),0)
     }
@@ -33,5 +116,4 @@ class MainActivity : AppCompatActivity() {
 
         if(data!=null) binding.imageView.setImageURI(data?.data)
     }
-
-}
+*/
---
+++ Jan_assignment/app/src/main/java/com/example/jan_assignment/MyAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.jan_assignment
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.LinearLayout
+import com.bumptech.glide.Glide
+import com.example.jan_assignment.databinding.ActivityMainBinding
+import org.jetbrains.anko.padding
+
+class MyAdapter(val context: Context, uriArr:ArrayList<String>) : BaseAdapter(){
+    private var items = ArrayList<String>()
+    init {
+        this.items = uriArr
+    }
+    override fun getCount(): Int {
+        return items.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return items[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(p: Int, convertView: View?, parent: ViewGroup?): View {
+        val imageView = ImageView(context)
+        val display = context.getResources().getDisplayMetrics()
+        imageView.padding = 2
+        imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+        imageView.layoutParams = LinearLayout.LayoutParams(display.widthPixels/3,display.widthPixels/3)
+        Glide.with(context).load(items[p]).into(imageView)
+        return imageView
+    }
+
+
+}
